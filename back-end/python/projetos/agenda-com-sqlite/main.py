580edcbc18dde219836448e9d9d9f9c80cfb43f8
Igor de Paula <igoroliveira7896@gmail.com>
2025-08-19 17:49:33 -0300

    otimização de código

--- back-end/python/projetos/agenda-com-sqlite/main.py
+++ back-end/python/projetos/agenda-com-sqlite/main.py
@@ -19,12 +19,11 @@ def listar_opcoes():
 ██╔══██║██║░░╚██╗██╔══╝░░██║╚████║██║░░██║██╔══██║
 ██║░░██║╚██████╔╝███████╗██║░╚███║██████╔╝██║░░██║
 ╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝
-          """)
+          \n""")
     print("Digite 1 para Cadastrar um contato.")
     print("Digite 2 para Listar os contatos.")
     print("Digite 3 para Atualizar um contato.")
-    print("Digite 4 para Apagar um contato.")
-    print()
+    print("Digite 4 para Apagar um contato.\n")
 
 def exibir_subtitulo(subtitulo):
     terminal.system('clear')
@@ -36,13 +35,15 @@ def cadastrar_contato():
     telefone = input("Digite o telefone do contato a ser cadastrado: ")
 
     cursor.execute("INSERT INTO contatos (nome, telefone) VALUES (?, ?)", (nome, telefone))
+    conexao.commit()
 
 def listar_contatos():
     exibir_subtitulo("Lista de Contatos:")
 
     cursor.execute("SELECT * FROM contatos")
     contatos = cursor.fetchall()
-    
+    conexao.commit()
+
     print()
     for contato in contatos:
         print(f"ID: {contato[0]} | Nome: {contato[1]} | Telefone: {contato[2]}")
@@ -53,30 +54,31 @@ def atualizar_contato():
     nome = input("Digite o nome do contato que você deseja alterar o número: ")
     novo_telefone = input("Digite o número correto do contato: ")
     cursor.execute("UPDATE contatos SET telefone = ? WHERE nome = ?", (novo_telefone, nome))
+    conexao.commit()
 
 def deletar_contato():
     exibir_subtitulo("Deletando um contato")
     nome = input("Digite o nome do contato a ser deletado: ")
-
     cursor.execute("DELETE FROM contatos WHERE nome = ?", (nome,))
+    conexao.commit()
 
 def escolher_opcao():
 
-    try:
-        opcao_escolhida = int(input("Digite a opção que deseja executar: "))
-        match opcao_escolhida:
-            case 1:
-                cadastrar_contato()
-            case 2:
-                listar_contatos()
-            case 3:
-                atualizar_contato()
-            case 4:
-                deletar_contato()
-            case _:
-                print("Opção inválida")
-    except:
-        print("Digite uma opção válida")
+        opcao_escolhida = input("Digite a opção que deseja executar: ")
+
+        if opcao_escolhida == "1":
+            cadastrar_contato()
+        elif opcao_escolhida == "2":
+            listar_contatos()
+        elif opcao_escolhida == "3":
+            atualizar_contato()
+        elif opcao_escolhida == "4":
+            deletar_contato()
+        else:
+            print("\nOpção inválida")
+            input("Pressione qualquer tecla para voltar\n")
+            listar_opcoes()
+            escolher_opcao()
 
 def main():
 
@@ -89,7 +91,6 @@ def main():
         escolher_opcao()
         continuar = input("Deseja fazer mais alguma coisa? ").lower()
     
-    conexao.commit()
     conexao.close()
 
 if __name__ == '__main__':
